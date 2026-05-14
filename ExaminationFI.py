434fdf817b086881bc42f7edc23001c7d9b9a0ad
bahar-bme <bah2mel@yahoo.com>
2026-05-13 23:05:17 -0700

    FIEXAM_commit5_13/5/2026

--- ExaminationFI.py
+++ ExaminationFI.py
@@ -74,7 +74,7 @@ def stratified_scaling(df, target_col, group_col='SEX_ASK_COM', inverse=False):
 
 
 ColumnNames = pd.read_csv(file_path, nrows=0).columns.tolist()
-print(ColumnNames)
+#print(ColumnNames)
 # df = pd.read_csv(file_path, dtype=optimized_dtypes)
 FIExamination = 47;DF = pd.DataFrame()
 
@@ -93,10 +93,16 @@ required_columns =['AGE_NMBR_COM',
                    'crt_mrtwout_corrans_com','CRT_LANG_COM',
                    'stp_dottime_ss_com','stp_coltime_ss_com','STP_STARTLANG_COM',
                    'bp_systolic_avg_com','bp_diastolic_avg_com','bp_pulse_avg_com',
-                   'bp_pulse_avg_com',
-                    ]
+                   'bp_pulse_avg_com','ecg_result_com','ecg_pq_interval_com','ecg_qrs_duration_com',
+                   'ecg_qtc_interval_com','ecg_p_axis_com','ecg_r_axis_com','ecg_t_axis_com',
+                   'ecg_p_duration_com','hwt_dbmi_com','whc_ratio_com',
+                   'dxa_wb_head_bmd_com','dxa_wb_larm_bmd_com','dxa_wb_rarm_bmd_com',
+                   'dxa_wb_lrib_bmd_com', 'dxa_wb_rrib_bmd_com','dxa_wb_t_s_bmd_com',
+                   'dxa_wb_l_s_bmd_com','dxa_wb_pelv_bmd_com','dxa_wb_lleg_bmd_com',
+                   'dxa_wb_rleg_bmd_com','DXA_WB_WBTOT_BMD_COM','DXA_OI_APDG_LEAN_MASS_H2_COM',
+                   'DXA_OI_TOTAL_PERCENT_FAT_COM']
 #'imt_r_avg_com','imt_l_avg_com'
-
+#'dxa_wb_wbtot_t_com'
 required_columns = [f.upper() for f in required_columns]
 
 required_columns.insert(0,'entity_id')
@@ -137,18 +143,18 @@ DF['cog_aft_score_2_com'.upper()] = stratified_scaling(df, target_col='cog_aft_s
                                                        group_col=strat_list, inverse=True)
 
 strat_list = ['SEX_ASK_COM', 'ED_HIGH_COM','FAS_F_LANG_COM']
-DF['FAS_F_SCORE'] = stratified_scaling(df, target_col='fas_f_score_com'.upper(),
+FAS_F_SCORE = stratified_scaling(df, target_col='fas_f_score_com'.upper(),
                                                        group_col=strat_list, inverse=True)
 
 strat_list = ['SEX_ASK_COM', 'ED_HIGH_COM','FAS_A_LANG_COM']
-DF['FAS_A_SCORE'] = stratified_scaling(df, target_col='fas_a_score_com'.upper(),
+FAS_A_SCORE = stratified_scaling(df, target_col='fas_a_score_com'.upper(),
                                                        group_col=strat_list, inverse=True)
 
 strat_list = ['SEX_ASK_COM', 'ED_HIGH_COM','FAS_S_LANG_COM']
-DF['FAS_S_SCORE'] = stratified_scaling(df, target_col='fas_s_score_com'.upper(),
+FAS_S_SCORE = stratified_scaling(df, target_col='fas_s_score_com'.upper(),
                                                        group_col=strat_list, inverse=True)                                                       
 
-DF['Controlled_Oral_Word_Association']=pd.concat([DF['FAS_A_SCORE'], DF['FAS_F_SCORE'], DF['FAS_S_SCORE']], axis=1).mean(axis=1,skipna=True)
+DF['Controlled_Oral_Word_Association']=pd.concat([FAS_A_SCORE,FAS_F_SCORE,FAS_S_SCORE], axis=1).mean(axis=1,skipna=True)
 
 df['Time_Based_Memory'] = df['tmt_rmd_com'.upper()].add(df['tmt_acc_com'.upper()], fill_value=0).add(df['tmt_itpexact_com'.upper()], fill_value=0)
 strat_list = ['SEX_ASK_COM', 'ED_HIGH_COM','TMT_LANG_COM']
@@ -173,19 +179,62 @@ DF['Stroop_Test_Interference_Time'] = stratified_scaling(df, target_col='Stroop_
 # Domain 3: Cardiac
 DF["bp_systolic_avg_com".upper()]  = (df["bp_systolic_avg_com".upper()]<90) | (df["bp_systolic_avg_com".upper()]>140)    
 DF["bp_diastolic_avg_com".upper()] = (df["bp_diastolic_avg_com".upper()]<=60)
-DF["bp_diastolic_avg_com".upper()] = (df["bp_diastolic_avg_com".upper()]<=60)
 df["pulse_pressure".upper()]       = df['bp_systolic_avg_com'.upper()]-df['bp_diastolic_avg_com'.upper()]
-DF['pulse_pressure'.upper()]       = (df['pulse_pressure'.upper()]<30) | (df['pulse_pressure'.upper()]>60)
-DF['bp_pulse_avg_com'.upper()]     = (df['bp_pulse_avg_com'.upper()]<60) | (df['bp_pulse_avg_com'.upper()]>99)
+DF['pulse_pressure'.upper()]       = (df['pulse_pressure'.upper()]<=30) | (df['pulse_pressure'.upper()]>60)
+DF['bp_pulse_avg_com'.upper()]     = (df['bp_pulse_avg_com'.upper()]<=60) | (df['bp_pulse_avg_com'.upper()]>99)
 #DF['imt_r_avg_com'.upper()]        = (df['imt_r_avg_com'.upper()]<0.5) | (df['imt_r_avg_com'.upper()]>0.8)
 #DF['imt_l_avg_com'.upper()]         = (df['imt_l_avg_com'.upper()]<0.5) | (df['imt_l_avg_com'.upper()]>0.8)
+DF['ecg_result_com'.upper()] = 'Nan'
+DF['ecg_result_com'.upper()].loc[(df['ecg_result_com'.upper()] == 4) | (df['ecg_result_com'.upper()] == 5)]= 1
+DF['ecg_result_com'.upper()].loc[df['ecg_result_com'.upper()] == 3] = 0.66
+DF['ecg_result_com'.upper()].loc[df['ecg_result_com'.upper()] == 2] = 0.33
+DF['ecg_result_com'.upper()].loc[df['ecg_result_com'.upper()] == 1] = 0
+DF['ecg_pq_interval_com'.upper()] = (df['ecg_pq_interval_com'.upper()]<=12) | (df['ecg_pq_interval_com'.upper()]>200)
+DF['ecg_qrs_duration_com'.upper()] = df['ecg_qrs_duration_com'.upper()] >= 100
+DF['ecg_qtc_interval_com'.upper()] = ((df["SEX_ASK_COM"]=="M") & (df['ecg_qtc_interval_com'.upper()]>430))|((df["SEX_ASK_COM"]=="F") & (df['ecg_qtc_interval_com'.upper()]>450))
+DF['ecg_p_axis_com'.upper()] = (df['ecg_p_axis_com'.upper()]<0) | (df['ecg_p_axis_com'.upper()]>75)
+DF['ecg_r_axis_com'.upper()] = (df['ecg_r_axis_com'.upper()]<-30) | (df['ecg_r_axis_com'.upper()]>90)
+DF['ecg_t_axis_com'.upper()] = (df['ecg_t_axis_com'.upper()]<0) | (df['ecg_t_axis_com'.upper()]>90)
+DF['ecg_p_duration_com'.upper()] = df['ecg_p_duration_com'.upper()]>120
+# Domain 4: Anthropometric measures
+DF['hwt_dbmi_com'.upper()] = 'Nan'
+DF['hwt_dbmi_com'.upper()].loc[(df['hwt_dbmi_com'.upper()]<=18.5)|(df['hwt_dbmi_com'.upper()]>=30)] = 1
+DF['hwt_dbmi_com'.upper()].loc[(df['hwt_dbmi_com'.upper()]>=25)&(df['hwt_dbmi_com'.upper()]<=29.9)] = 0.5 
+DF['hwt_dbmi_com'.upper()].loc[(df['hwt_dbmi_com'.upper()]>=18.5)&(df['hwt_dbmi_com'.upper()]<=24.9)] = 0
+DF['whc_ratio_com'.upper()] = ((df['SEX_ASK_COM']=='M')&(df['whc_ratio_com'.upper()]>0.9)) | ((df['SEX_ASK_COM']=='F')&(df['whc_ratio_com'.upper()]>0.85))
+DXA_WB_WBTOT_BMD_T_COM = (df['DXA_WB_WBTOT_BMD_COM']-df['DXA_WB_WBTOT_BMD_COM'].mean()) /df['DXA_WB_WBTOT_BMD_COM'].std()
+DF['DXA_WB_WBTOT_T_COM'.upper()] = 'Nan'
+DF['DXA_WB_WBTOT_T_COM'.upper()].loc[DXA_WB_WBTOT_BMD_T_COM<=-2.5]=1
+DF['DXA_WB_WBTOT_T_COM'.upper()].loc[(DXA_WB_WBTOT_BMD_T_COM>=-2.5)&(DXA_WB_WBTOT_BMD_T_COM<1)]=0.5
+DF['DXA_WB_WBTOT_T_COM'.upper()].loc[DXA_WB_WBTOT_BMD_T_COM<=-1]=0
+Osteoporosis =['dxa_wb_head_bmd_com'.upper(),'dxa_wb_larm_bmd_com'.upper(),
+               'dxa_wb_rarm_bmd_com'.upper(),'dxa_wb_lrib_bmd_com'.upper(),
+               'dxa_wb_rrib_bmd_com'.upper(),'dxa_wb_t_s_bmd_com'.upper(),
+               'dxa_wb_l_s_bmd_com'.upper(),'dxa_wb_pelv_bmd_com'.upper(),
+               'dxa_wb_lleg_bmd_com'.upper(),'dxa_wb_rleg_bmd_com'.upper()]
 
+Osteoporosis_T = (df[Osteoporosis] - df[Osteoporosis].mean()) / df[Osteoporosis].std()
+Osteoporosis_T_TF = Osteoporosis_T<=-2.5
+Osteoporosis_T_Count = Osteoporosis_T_TF.sum(axis=1)
+DF['DXA_Osteoporosis_BMD_T']='Nan'
+DF['DXA_Osteoporosis_BMD_T'].loc[Osteoporosis_T_Count>=2]=1
+DF['DXA_Osteoporosis_BMD_T'].loc[Osteoporosis_T_Count==1]=0.5
+DF['DXA_Osteoporosis_BMD_T'].loc[Osteoporosis_T_Count==0]=0
+DF['DXA_OI_APDG_LEAN_MASS_H2_COM'] = stratified_scaling(df, 'DXA_OI_APDG_LEAN_MASS_H2_COM', inverse=True)
+DF['DXA_OI_TOTAL_PERCENT_FAT_COM'] = stratified_scaling(df, 'DXA_OI_TOTAL_PERCENT_FAT_COM', inverse=False)
 
 print(DF)
+
 #print(K)
-print(df['Stroop_Test_Interference_Time'])
+#print(df['ecg_result_com'.upper()])
 #print(max(df['tmt_itpexact_com'.upper()]))
-import matplotlib.pyplot as plt
-import numpy as np
-plt.hist(DF['Stroop_Test_Interference_Time'], bins=20,color='skyblue', edgecolor='black')
-plt.show()
+#import matplotlib.pyplot as plt
+#import numpy as np
+#plt.hist(DF['Stroop_Test_Interference_Time'], bins=20,color='skyblue', edgecolor='black')
+#plt.show()
+
+"""
+# Select boolean columns and overwrite them with 1 and 0
+bool_cols = df.select_dtypes(include='bool').columns
+df[bool_cols] = df[bool_cols].astype(int)
+"""
